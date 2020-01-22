6e85dd2503f64ed1991723379e1299f747a8782c
pwoodsum <pwoodsum@autonodyne.com>
2020-01-22 18:50:09 -0500

    Added basic WASD character movement. Added a temporary player sprite, changed robots target to the player instead of the mouse.

---
+++ Z-Team Game 1/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float moveSpeed = 30.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.z = Input.GetAxisRaw("Vertical");
+
+        moveDirection.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = transform.position + (moveDirection * moveSpeed * Time.fixedDeltaTime);
+        
+        if (moveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Euler(0, Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + 180.0f, 0);
+        }
+    }
+}
--- Z-Team Game 1/Assets/Scripts/Robot.cs
+++ Z-Team Game 1/Assets/Scripts/Robot.cs
@@ -5,12 +5,15 @@ using UnityEngine.AI;
 
 public class Robot : MonoBehaviour
 {
-
     NavMeshAgent agent;
+
+    Transform playerTransform;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        playerTransform = GameObject.Find("Player").transform;
     }
 
     // Start is called before the first frame update
@@ -21,7 +24,14 @@ public class Robot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 pnt = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+        Vector3 pnt = Vector3.zero;
+
+        if (playerTransform)
+        {
+            pnt = playerTransform.position;
+        }
+        
+        //Vector3 pnt = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
         agent.destination = pnt;
         pnt.z = -1;
     }
