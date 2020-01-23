4370aa763be68e1927d33a7bc07602435689ad48
pwoodsum <pwoodsum@autonodyne.com>
2020-01-22 19:47:22 -0500

    Fixed merge issue I had. Added the player sprite back to the Unity scene. Added bound collisions to keep the player on the ground.

--- Z-Team Game 1/Assets/Scripts/Player.cs
+++ Z-Team Game 1/Assets/Scripts/Player.cs
@@ -8,6 +8,28 @@ public class Player : MonoBehaviour
 
     private Vector3 moveDirection = Vector3.zero;
 
+    private float spriteWidth;
+
+    private float leftBound;
+    private float topBound;
+    private float rightBound;
+    private float bottomBound;
+
+    private void Start()
+    {
+        spriteWidth = transform.Find("Sprite").GetComponent<SpriteRenderer>().size.x;
+
+        Transform groundTransform = GameObject.Find("Ground").transform;
+        MeshRenderer groundRenderer = groundTransform.GetComponent<MeshRenderer>();
+
+        float spriteHalfWidth = spriteWidth / 2;
+
+        leftBound = groundTransform.position.x - groundRenderer.bounds.extents.x + spriteHalfWidth;
+        rightBound = groundTransform.position.x + groundRenderer.bounds.extents.x - spriteHalfWidth;
+        bottomBound = groundTransform.position.z - groundRenderer.bounds.extents.z + spriteHalfWidth;
+        topBound = groundTransform.position.z + groundRenderer.bounds.extents.z - spriteHalfWidth;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -25,5 +47,31 @@ public class Player : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0, Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + 180.0f, 0);
         }
+
+        CheckBounds();
+    }
+
+    private void CheckBounds()
+    {
+        Vector3 newTransform = transform.position;
+
+        if (transform.position.x < leftBound)
+        {
+            newTransform.x = leftBound;
+        }
+        else if (transform.position.x > rightBound)
+        {
+            newTransform.x = rightBound;
+        }
+        if (transform.position.z < bottomBound)
+        {
+            newTransform.z = bottomBound;
+        }
+        else if (transform.position.z > topBound)
+        {
+            newTransform.z = topBound;
+        }
+
+        transform.position = newTransform;
     }
 }
